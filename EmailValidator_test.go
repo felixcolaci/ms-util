1a0322a022c50a90e59e17fcf0c2227ce90030dd
fcolaci <felix.colaci@onlinehome.de>
2018-03-24 18:17:38 +0100

    migrated package

---
+++ EmailValidator_test.go
@@ -0,0 +1,27 @@
+package util
+
+import "testing"
+
+var emails = []struct{
+	email string
+	result bool
+}{
+	{"felix.colaci@onlinehome.de", true},
+	{"carlos@example.co.uk", true},
+	{"carlos.com", false},
+	{"carlos", false},
+	{"carlos@example", true},
+}
+
+func TestValidateEmail(t *testing.T) {
+
+	for _, tt := range emails {
+
+		result := ValidateEmail(tt.email)
+		if result != tt.result {
+			t.Errorf("Email validation failed for %v | Expected %v got %v", tt.email, tt.result, result)
+		}
+
+	}
+
+}
