c276241dca8f6ba753d0c53ba2ab38a572b66cf2
fcolaci <felix.colaci@onlinehome.de>
2018-05-03 17:55:15 +0200

    fix tests

--- ConfigManager_test.go
+++ ConfigManager_test.go
@@ -7,71 +7,71 @@ func TestNewManagedConfigurationFromFile(t *testing.T) {
 
 	conf := NewManagedConfiguration("testdata/complete.yaml")
 
-	if conf.configuration.Base.Port != 8080 {
-		t.Errorf("Application port mismatch from yaml. Expected %v got %v", 8080, conf.configuration.Base.Port)
+	if conf.Configuration.Base.Port != 8080 {
+		t.Errorf("Application port mismatch from yaml. Expected %v got %v", 8080, conf.Configuration.Base.Port)
 	}
 
-	if conf.configuration.Base.MgmtPort != 9000 {
-		t.Errorf("Management port mismatch from yaml. Expected %v got %v", 9000, conf.configuration.Base.MgmtPort)
+	if conf.Configuration.Base.MgmtPort != 9000 {
+		t.Errorf("Management port mismatch from yaml. Expected %v got %v", 9000, conf.Configuration.Base.MgmtPort)
 	}
 
-	if conf.configuration.Postgres.Port != 5432 {
-		t.Errorf("postgres port mismatch. Expected %v got %v", 5432, conf.configuration.Postgres.Port)
+	if conf.Configuration.Postgres.Port != 5432 {
+		t.Errorf("postgres port mismatch. Expected %v got %v", 5432, conf.Configuration.Postgres.Port)
 	}
 
 }
 func TestNewManagedConfigurationWithDefaults(t *testing.T) {
 	conf := NewManagedConfiguration()
 
-	if conf.configuration.Base.Port != 8080 {
-		t.Errorf("Application port mismatch from yaml. Expected %v got %v", 8080, conf.configuration.Base.Port)
+	if conf.Configuration.Base.Port != 8080 {
+		t.Errorf("Application port mismatch from yaml. Expected %v got %v", 8080, conf.Configuration.Base.Port)
 	}
 
-	if conf.configuration.Postgres.Port != 5432 {
-		t.Errorf("postgres port mismatch. Expected %v got %v", 5432, conf.configuration.Postgres.Port)
+	if conf.Configuration.Postgres.Port != 5432 {
+		t.Errorf("postgres port mismatch. Expected %v got %v", 5432, conf.Configuration.Postgres.Port)
 	}
 }
 
 func TestNewManagedConfigurationBaseOnly(t *testing.T) {
 	conf := NewManagedConfiguration("testdata/base-only.yaml")
 
-	if conf.configuration.Base.Port != 8080 {
-		t.Errorf("Application port mismatch from yaml. Expected %v got %v", 8080, conf.configuration.Base.Port)
+	if conf.Configuration.Base.Port != 8080 {
+		t.Errorf("Application port mismatch from yaml. Expected %v got %v", 8080, conf.Configuration.Base.Port)
 	}
 
-	if conf.configuration.Base.MgmtPort != 9000 {
-		t.Errorf("Management port mismatch from yaml. Expected %v got %v", 9000, conf.configuration.Base.MgmtPort)
+	if conf.Configuration.Base.MgmtPort != 9000 {
+		t.Errorf("Management port mismatch from yaml. Expected %v got %v", 9000, conf.Configuration.Base.MgmtPort)
 	}
 
-	if conf.configuration.Base.BasePath != "/path" {
-		t.Errorf("basepath mismatch. Expected %v got %v", "/path", conf.configuration.Base.BasePath)
+	if conf.Configuration.Base.BasePath != "/path" {
+		t.Errorf("basepath mismatch. Expected %v got %v", "/path", conf.Configuration.Base.BasePath)
 	}
 }
 func TestNewManagedConfigurationPostgresOnly(t *testing.T) {
 	conf := NewManagedConfiguration("testdata/postgres.yaml")
 
-	if conf.configuration.Postgres.Host != "example.com" {
-		t.Errorf("postgres host mismatch from yaml. Expected %v got %v", "example.com", conf.configuration.Postgres.Host)
+	if conf.Configuration.Postgres.Host != "example.com" {
+		t.Errorf("postgres host mismatch from yaml. Expected %v got %v", "example.com", conf.Configuration.Postgres.Host)
 	}
-	if conf.configuration.Postgres.Database != "testdb" {
-		t.Errorf("postgres database mismatch from yaml. Expected %v got %v", "testdb", conf.configuration.Postgres.Database)
+	if conf.Configuration.Postgres.Database != "testdb" {
+		t.Errorf("postgres database mismatch from yaml. Expected %v got %v", "testdb", conf.Configuration.Postgres.Database)
 	}
-	if conf.configuration.Postgres.MaxCon != 100 {
-		t.Errorf("postgres connection mismatch from yaml. Expected %v got %v", 100, conf.configuration.Postgres.MaxCon)
+	if conf.Configuration.Postgres.MaxCon != 100 {
+		t.Errorf("postgres connection mismatch from yaml. Expected %v got %v", 100, conf.Configuration.Postgres.MaxCon)
 	}
 }
 
 func TestNewManagedConfigurationMongoOnly(t *testing.T) {
 	conf := NewManagedConfiguration("testdata/mongo.yaml")
 
-	if conf.configuration.Mongo.Host != "example.com" {
-		t.Errorf("mongo host mismatch from yaml. Expected %v got %v", "example.com", conf.configuration.Mongo.Host)
+	if conf.Configuration.Mongo.Host != "example.com" {
+		t.Errorf("mongo host mismatch from yaml. Expected %v got %v", "example.com", conf.Configuration.Mongo.Host)
 	}
-	if conf.configuration.Mongo.Database != "testdb" {
-		t.Errorf("Mongo database mismatch from yaml. Expected %v got %v", "testdb", conf.configuration.Mongo.Database)
+	if conf.Configuration.Mongo.Database != "testdb" {
+		t.Errorf("Mongo database mismatch from yaml. Expected %v got %v", "testdb", conf.Configuration.Mongo.Database)
 	}
-	if conf.configuration.Mongo.Port != 27000 {
-		t.Errorf("Mongo port mismatch from yaml. Expected %v got %v", 27000, conf.configuration.Mongo.Port)
+	if conf.Configuration.Mongo.Port != 27000 {
+		t.Errorf("Mongo port mismatch from yaml. Expected %v got %v", 27000, conf.Configuration.Mongo.Port)
 	}
 }
 
@@ -79,8 +79,8 @@ func TestNewManagedConfigurationCachingOnly(t *testing.T) {
 
 	conf := NewManagedConfiguration("testdata/caching.yaml")
 
-	if conf.configuration.Cache.EnableCaching {
-		t.Errorf("caching mismatch from yaml. Expected %v got %v", false, conf.configuration.Cache.EnableCaching)
+	if conf.Configuration.Cache.EnableCaching {
+		t.Errorf("caching mismatch from yaml. Expected %v got %v", false, conf.Configuration.Cache.EnableCaching)
 	}
 
 }
@@ -89,8 +89,8 @@ func TestNewManagedConfigurationSessionOnly(t *testing.T) {
 
 	conf := NewManagedConfiguration("testdata/session.yaml")
 
-	if conf.configuration.Session.AutogenerateKeyset{
-		t.Errorf("session keyset mismatch from yaml. Expected %v got %v", false, conf.configuration.Session.AutogenerateKeyset)
+	if conf.Configuration.Session.AutogenerateKeyset{
+		t.Errorf("session keyset mismatch from yaml. Expected %v got %v", false, conf.Configuration.Session.AutogenerateKeyset)
 	}
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # UTIL #
+[![Build Status](https://travis-ci.org/felixcolaci/ms-util.svg?branch=master)](https://travis-ci.org/felixcolaci/ms-util)
 
 common utils
